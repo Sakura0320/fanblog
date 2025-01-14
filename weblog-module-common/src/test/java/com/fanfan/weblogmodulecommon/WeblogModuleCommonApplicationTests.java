c3364c66e177b68f9f64d33050d1cf0e7b3a0139
lyf <1692078540@qq.com>
2025-01-14 14:41:09 +0800

    initial

---
+++ weblog-module-common/src/test/java/com/fanfan/weblogmodulecommon/WeblogModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanfan.weblogmodulecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ weblog-web/src/main/java/com/fanfan/weblogweb/WeblogWebApplication.java
@@ -0,0 +1,14 @@
+package com.fanfan.weblogweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+public class WeblogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblogWebApplication.class, args);
+    }
+
+}
